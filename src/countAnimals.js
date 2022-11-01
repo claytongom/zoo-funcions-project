0b9912e5c222e9a5a389bea73844a85db3124fc0
claytongom <claytoncnx@gmail.com>
2022-11-01 12:27:19 -0300

    resolução requisto 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const newObj = {};
+    species.forEach((specie) => {
+      newObj[specie.name] = specie.residents.length;
+    });
+    return newObj;
+  } if (Object.keys(animal).length === 1) {
+    return species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+  return species
+    .find((specie) => specie.name === animal.specie)
+    .residents.filter((resident) => resident.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
