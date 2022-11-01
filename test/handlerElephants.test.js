3cce35e86430c7f404d7d18534c1fd14881e5224
claytongom <claytoncnx@gmail.com>
2022-11-01 14:11:01 -0300

    resolução requisito 7 - coverage 92%

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -2,6 +2,7 @@ const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
   it('Confere parâmetros de argumentos', () => {
+    expect(handlerElephants()).toBe(undefined);
     expect(handlerElephants('count')).toBe(4);
     expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
     expect(handlerElephants('averageAge')).toBe(10.5);
@@ -10,3 +11,5 @@ describe('Testes da função HandlerElephants', () => {
     expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
   });
 });
+
+
