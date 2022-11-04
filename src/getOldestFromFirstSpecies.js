1d179fda2fc29c1f2351b2216faec405057d9e9e
claytongom <claytoncnx@gmail.com>
2022-11-04 17:36:26 -0300

    resolução requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+const { species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalId = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animal = species.find((specie) => specie.id === animalId).residents;
+
+  return Object.values(animal.sort((a, b) => a.age - b.age)[animal.length - 1]);
 }
 
 module.exports = getOldestFromFirstSpecies;
