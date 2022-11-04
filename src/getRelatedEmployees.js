1878da9646271bf7a90c622d00edc213d93313fa
claytongom <claytoncnx@gmail.com>
2022-11-04 16:09:14 -0300

    resolução requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+// hof some verifica se id = parametro
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
-
+// hof filter array employees, map retornar nome sobrenome empregados que gerente é responsavel
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.filter((employee) => employee.managers.includes(managerId))
+      .map((el) => `${el.firstName} ${el.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
